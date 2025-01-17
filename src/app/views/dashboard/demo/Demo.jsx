d614d5cc21fcab8c08ce70aa0c2efb654dd3077a
Raunak singh <Raunak@gmail.com>
2025-01-17 10:55:43 +0530

    Updated Code

--- src/app/views/dashboard/demo/Demo.jsx
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-
-// export const Demo = () => {
-//   return (
-//     <h1>demo checking</h1>
-//   )
-// }
-
--- src/app/views/material-kit/alertDashboard/AlertDashboard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AlertDashboard() {
-  return (
-    <div>AlertDashboard</div>
-  )
-}
-
-export default AlertDashboard

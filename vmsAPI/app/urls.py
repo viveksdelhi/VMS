1809e8d562db0f3fc0087b038f48d7c61e23050e
Harshit Kumar Rai <harshit.rai001@gmail.com>
2025-02-10 14:48:05 +0530

    routers updated

--- vmsAPI/app/urls.py
+++ vmsAPI/app/urls.py
@@ -4,20 +4,6 @@ from django.urls import path
 
 
 router = DefaultRouter()
-# router.register(r'ANPRStatus', AnprstatusViewSet)
-# router.register(r'CameraAlertStatus', CameraalertstatussViewSet) 
-# router.register(r'CameraAlert', CameraalertsViewSet)
-# router.register(r'CameraIPList', CameraiplistsViewSet)
-# router.register(r'Camera', CamerasViewSet)
-# router.register(r'Group', GroupsViewSet)
-# router.register(r'NVR', NvrViewSet)
-# router.register(r'NumberPlateDetection', NumberplatedetectionsViewSet)
-# router.register(r'NumberPlateReadedData', ReadedvehiclenoplatesViewSet)
-# router.register(r'Role', RolesViewSet)
-# router.register(r'user', UsersViewSet)
-# router.register(r'VehicleDetection', VehicledetectionsViewSet)
-# router.register(r'VideoAnalytic', VideoanalyticsViewSet)
-
 
 router.register(r'ANPRStatus', AnprstatusViewSet, basename='anprstatus')
 router.register(r'CameraAlertStatus', CameraalertstatussViewSet, basename='cameraalertstatuss') 
@@ -33,7 +19,7 @@ router.register(r'user', UsersViewSet, basename='users')
 router.register(r'VehicleDetection', VehicledetectionsViewSet, basename='vehicledetections')
 router.register(r'VideoAnalytic', VideoanalyticsViewSet, basename='videoanalytics')
 
-# urlpatterns = router.urls
+
 urlpatterns = router.urls +[
   path('Auth/login', LoginView.as_view(), name='auth-login'),
 ]

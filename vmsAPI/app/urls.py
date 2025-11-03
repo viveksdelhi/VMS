b3cd6a29374016a89a2ec2b4a23395163fc825f7
Shubham <89654260+ShubhamLokesh@users.noreply.github.com>
2025-11-03 15:49:43 +0530

    Update urls.py

--- vmsAPI/app/urls.py
+++ vmsAPI/app/urls.py
@@ -20,6 +20,7 @@ router.register(r'Role', RolesViewSet, basename='roles')
 router.register(r'user', UsersViewSet, basename='users')
 router.register(r'VehicleDetection', VehicledetectionsViewSet, basename='vehicledetections')
 router.register(r'VideoAnalytic', VideoanalyticsViewSet, basename='videoanalytics')
+router.register(r'VideoAnalyticCount', VideoanalyticsCountViewSet, basename='videoanalyticscount')
 
 
 urlpatterns = router.urls +[

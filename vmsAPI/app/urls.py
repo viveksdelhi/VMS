c4e73d960b3ce9e69385183c3f355afa62998a5e
Shubham <89654260+ShubhamLokesh@users.noreply.github.com>
2025-11-03 16:40:48 +0530

    Update urls.py

--- vmsAPI/app/urls.py
+++ vmsAPI/app/urls.py
@@ -20,7 +20,7 @@ router.register(r'Role', RolesViewSet, basename='roles')
 router.register(r'user', UsersViewSet, basename='users')
 router.register(r'VehicleDetection', VehicledetectionsViewSet, basename='vehicledetections')
 router.register(r'VideoAnalytic', VideoanalyticsViewSet, basename='videoanalytics')
-router.register(r'CameraalertsCount', Cameraalerts, basename='Cameraalertscount')
+router.register(r'CameraalertsCount', CameraalertsCountViewSet, basename='Cameraalertscount')
 
 
 
